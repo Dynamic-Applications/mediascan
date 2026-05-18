f01c980e82dfd99b3571f08eb3ad995151bf8625
Mirage Kamran <mirage.kamran.pkt@gmail.com>
2026-05-17 22:57:56 -0400

    a few changes on UI texts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,18 +1,22 @@
 export const navItems = [
-  {
-    name: "Login",
-    href: "/",
-  },
-  {
-    name: "Playlists",
-    href: "/playlists",
-  },
-  {
-    name: "Profile",
-    href: "/profile",
-  },
-  {
-    name: "Contact",
-    href: "/contact",
-  },
+    {
+        name: "Home",
+        href: "/",
+    },
+    {
+        name: "Login",
+        href: "/login",
+    },
+    {
+        name: "Playlists",
+        href: "/playlists",
+    },
+    {
+        name: "Profile",
+        href: "/profile",
+    },
+    {
+        name: "Contact",
+        href: "/contact",
+    },
 ];
